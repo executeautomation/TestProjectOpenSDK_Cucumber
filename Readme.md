06626214c3e8c12b86495a055fbc022c5bf02b81
ExecuteAutomation <kartmcad@gmail.com>
2020-07-15 18:11:39 +1200

    Added first initial commit
    Added first initial commit

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# SeleniumWithCucumber
+This is an sample project to demonstrate how to work with Selenium and cucumber for Java
+
+~~~~
+# Important
+Make sure you have JAVA 10 or above installed to run this test
+~~~~
+
+# Upgraded to Cucumber 4
+The code is now upgraded to Cucumber 4 with new
+
+# Upgraded to Cucumber-JVM 5
+The code is now upgraded to Cucumber 4 with new
+
+1. Libraries support
+2. Test Runner support
+3. Hooks (@BeforeStep and @AfterStep)
+4. DataTable code change
+
+## More Information
+The complete code base development video is available in ExecuteAutomation YouTube channel 
+https://www.youtube.com/playlist?list=PL6tu16kXT9Pqr70SZlwcmTSAfOw_0Qj3R
+
+
+## Cucumber BDD video Series (YouTube)
+https://www.youtube.com/playlist?list=PL6tu16kXT9PpteusHGISu_lHcV6MbBtA6
+
+## Cucumber BDD video Series (Udemy)
+https://www.udemy.com/cucumber-with-selenium/
+
+### For more articles and videos
+Visit http://www.executeautomation.com
---
+++ src/test/java/steps/Hook.java
@@ -0,0 +1,82 @@
+package steps;
+
+import Base.BaseUtil;
+
+
+//import cucumber.api.PickleStepTestStep;
+//import cucumber.api.TestCase;
+//import gherkin.pickles.PickleStep;
+//import io.cucumber.core.api.Scenario;
+
+
+import io.cucumber.java.*;
+import io.testproject.sdk.DriverBuilder;
+import io.testproject.sdk.drivers.web.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+
+
+/**
+ * Created by Karthik on 31/01/2019.
+ */
+
+public class Hook extends BaseUtil{
+
+
+    private BaseUtil base;
+
+    public Hook(BaseUtil base) {
+        this.base = base;
+    }
+
+    @Before
+    public void InitializeTest(Scenario scenario) {
+
+
+        base.scenarioDef = base.features.createNode(scenario.getName());
+
+        System.out.println("Opening the browser : Firefox");
+
+        /*System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
+        base.Driver = new FirefoxDriver();*/
+
+
+        //Chrome driver
+//        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
+//        base.Driver = new ChromeDriver();
+
+//        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
+//        base.Driver = new ChromeDriver();
+
+        base.Driver = new DriverBuilder<ChromeDriver>(new ChromeOptions())
+                .withProjectName("My Test Project ")
+                .withJobName("First Test Job")
+                .withToken("U--LKi5tve35kNidu4uRp5IzAWrhL7PNVp3jEn4Y55U1")
+                .build(ChromeDriver.class);
+
+    }
+
+
+    @After
+    public void TearDownTest(Scenario scenario) {
+        if (scenario.isFailed()) {
+            //Take screenshot logic goes here
+            System.out.println(scenario.getName());
+        }
+        System.out.println("Closing the browser : MOCK");
+        base.Driver.quit();
+    }
+
+    @BeforeStep
+    public void BeforeEveryStep(Scenario scenario) {
+        System.out.println("Before every step " + scenario.getId());
+
+        //((PickleStep)((PickleStepTestStep)
+    }
+
+    @AfterStep
+    public void AfterEveryStep(Scenario scenario) throws NoSuchFieldException, IllegalAccessException {
+        //System.out.println("Before every step " + stepTestStep.getId());
+    }
+
+}
